f3a0d2a5bbe7a827f56585869273423c0a2ac4f1
Whitney Chase <50594549+WhitneymChase@users.noreply.github.com>
2019-06-14 12:12:18 -0600

    Update Mainactivity.java

--- Mainactivity.java
+++ Mainactivity.java
@@ -1,4 +1,14 @@
- @Override
+package com.example.bluebalm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+ 
+@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
