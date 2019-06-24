5f8b25c58c956c0896dbda7d217055260e9ec361
Kraig Taylor <tay15028@byui.edu>
2019-06-24 16:08:24 -0600

    Made a project branch and added files for the whole project

---
+++ app/src/main/java/com/assignment/bluebalm/Instruction.java
@@ -0,0 +1,16 @@
+package com.example.bluebalm;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Instructions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructions);
+        MediaPlayer song = MediaPlayer.create(this,R.raw.water);
+        song.start();
+    }
+}
