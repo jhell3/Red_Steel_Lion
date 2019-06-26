5ced67ada8b7317477b3bb4d391c23693958030c
Whitney Chase <50594549+WhitneymChase@users.noreply.github.com>
2019-06-26 12:23:29 -0600

    Update Instruction.java

--- Instruction.java
+++ Instruction.java
@@ -1,16 +1,39 @@
 package com.example.bluebalm;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
 public class Instructions extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions);
-        MediaPlayer song = MediaPlayer.create(this,R.raw.water);
-        song.start();
+        Intent intent = getIntent();
+        String musicSelect = intent.getStringExtra("song");
+        switch (musicSelect)
+        {
+            case "instrumental":
+                MediaPlayer song = MediaPlayer.create(this,R.raw.instramental);
+                song.start();
+                break;
+            case "whitenoise":
+                song = MediaPlayer.create(this,R.raw.whitenoise);
+                song.start();
+                break;
+            case "water":
+                song = MediaPlayer.create(this,R.raw.water);
+                song.start();
+                break;
+            case "nature":
+                song = MediaPlayer.create(this,R.raw.nature);
+                song.start();
+                break;
+        }
     }
 }
