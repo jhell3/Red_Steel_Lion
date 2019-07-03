ffbc9a52dc93931a51b5ef449fd0c112d1964455
jhell3 <50592962+jhell3@users.noreply.github.com>
2019-07-03 11:28:04 -0600

    Update Instructions.java

--- Instructions.java
+++ Instructions.java
@@ -6,9 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-
+/**
+ * Last activity. Plays music when created, and has various buttons to pause, play, and stop.
+ */
 public class Instructions extends AppCompatActivity {
-   private MediaPlayer song;
+    private MediaPlayer song;
 
 
     @Override
@@ -17,35 +19,47 @@ public class Instructions extends AppCompatActivity {
         setContentView(R.layout.activity_instructions);
         Intent intent = getIntent();
         String musicSelect = intent.getStringExtra("song");
-       switch (musicSelect)
-       {
-           case "instrumental":
-               song = MediaPlayer.create(this,R.raw.instramental);
-               song.start();
-               break;
-           case "whitenoise":
-               song = MediaPlayer.create(this,R.raw.whitenoise);
-               song.start();
-               break;
-          case "water":
-               song = MediaPlayer.create(this,R.raw.water);
-               song.start();
-               break;
-           case "nature":
-               song = MediaPlayer.create(this,R.raw.nature);
-               song.start();
-               break;        }
+        switch (musicSelect)
+        {
+            case "instrumental":
+                song = MediaPlayer.create(this,R.raw.instramental);
+                song.start();
+                break;
+            case "whitenoise":
+                song = MediaPlayer.create(this,R.raw.whitenoise);
+                song.start();
+                break;
+            case "water":
+                song = MediaPlayer.create(this,R.raw.water);
+                song.start();
+                break;
+            case "nature":
+                song = MediaPlayer.create(this,R.raw.nature);
+                song.start();
+                break;        }
     }
+
+    /**
+     *
+     * @param button9 stop the music
+     */
     public void onStop(View button9){
         song.stop();
     }
     @Override
+    /**
+     * onDestroy is used to stop the song. 
+     */
     public void onDestroy() {
         super.onDestroy();
         if (song != null) song.release();
     }
-   
-   public void pause(View button10){
+
+    /**
+     * 
+     * @param button10 pause and play music. 
+     */
+    public void pause(View button10){
         if (song.isPlaying()){
             song.pause();
         }
