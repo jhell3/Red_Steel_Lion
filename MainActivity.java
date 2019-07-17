7971067e3648735ec6c915dd0e527cea7eb25c64
Whitney Chase <50594549+WhitneymChase@users.noreply.github.com>
2019-07-17 12:24:00 -0600

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -7,58 +7,58 @@ import android.view.View;
 import android.content.Intent;
 
 /**
- * First activity. used to select the music type. 
+ * First activity. used to select the music type.
  */
 public class MainActivity extends AppCompatActivity {
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_main);
-        }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
-        /**
-         *
-         * @param button used to select the instrumental music
-         */
-        public void onInstrumental(View button){
+    /**
+     *
+     * @param button used to select the instrumental music
+     */
+    public void onInstrumental(View button){
 
-                Intent intent = new Intent(this, Music_Time_Selection.class);
-                intent.putExtra("song", "instrumental");
-                startActivity(intent);
-        }
+        Intent intent = new Intent(this, Music_Time_Selection.class);
+        intent.putExtra("song", "instrumental");
+        startActivity(intent);
+    }
 
-        /**
-         * 
-         * @param button2 used to select white noise
-         */
-        public void onWhiteNoise(View button2){
-                Intent intent = new Intent(this, Music_Time_Selection.class);
-                intent.putExtra("song", "whitenoise");
-                startActivity(intent);
-        }
+    /**
+     *
+     * @param button2 used to select white noise
+     */
+    public void onWhiteNoise(View button2){
+        Intent intent = new Intent(this, Music_Time_Selection.class);
+        intent.putExtra("song", "whitenoise");
+        startActivity(intent);
+    }
 
-        /**
-         * 
-         * @param button3 used to select water
-         */
-        public void onWaterSounds(View button3){
+    /**
+     *
+     * @param button3 used to select water
+     */
+    public void onWaterSounds(View button3){
 
-                Intent intent = new Intent(this, Music_Time_Selection.class);
-                intent.putExtra("song","water");
-                startActivity(intent);
-        }
+        Intent intent = new Intent(this, Music_Time_Selection.class);
+        intent.putExtra("song","water");
+        startActivity(intent);
+    }
 
-        /**
-         * 
-         * @param button4 used to select Nature sounds. 
-         */
-        public void onNatureSounds(View button4){
+    /**
+     *
+     * @param button4 used to select Nature sounds.
+     */
+    public void onNatureSounds(View button4){
 
-                Intent intent = new Intent(this, Music_Time_Selection.class);
-                intent.putExtra("song", "nature");
-                startActivity(intent);
-        }
+        Intent intent = new Intent(this, Music_Time_Selection.class);
+        intent.putExtra("song", "nature");
+        startActivity(intent);
+    }
 
 
 }
